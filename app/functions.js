f3eeaa9634b67eec7638f4bf375643098c62e6b3
Luke Jervis <88159203+viper14lk@users.noreply.github.com>
2022-11-01 18:47:58 +0000

    update

--- app/functions.js
+++ app/functions.js
@@ -1,39 +1,43 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray: function(fn, arr) {
+    argsAsArray: function (fn, arr) {
+        return fn(...arr);
+    },
 
-  },
+    speak: function (fn, obj) {
+        return fn.call(obj);
+    },
 
-  speak: function(fn, obj) {
+    functionFunction: function (str) {
+        return function (str2) {
+            return str + ", " + str2;
+        };
+    },
 
-  },
+    makeClosures: function (arr, fn) {},
 
-  functionFunction: function(str) {
+    partial: function (fn, str1, str2) {
+        return function (str3) {
+            return fn(str1, str2, str3);
+        };
+    },
 
-  },
+    useArguments: function (a, b = 0, c = 0, d = 0) {
+        return a + b + c + d;
+    },
 
-  makeClosures: function(arr, fn) {
+    callIt: function (fn) {},
 
-  },
+    partialUsingArguments: function (fn) {
+        const argv = Array.prototype.slice.call(arguments, 1, arguments.length);
+        return function () {
+            const more_argv = argv.concat(
+                Array.prototype.slice.call(arguments)
+            );
+            return fn.apply(null, more_argv);
+        };
+    },
 
-  partial: function(fn, str1, str2) {
-
-  },
-
-  useArguments: function() {
-
-  },
-
-  callIt: function(fn) {
-
-  },
-
-  partialUsingArguments: function(fn) {
-
-  },
-
-  curryIt: function(fn) {
-
-  }
+    curryIt: function (fn) {}
 };
